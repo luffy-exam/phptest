911ef277123652c40ccc7fda85310928874bd54a
luffy-exam <fake79019@gmail.com>
2025-06-16 19:24:08 +0530

    Delete New folder directory

--- New folder/dashboard.php
+++
@@ -1,9 +0,0 @@
-<?php
-// dashboard.php
-session_start();
-if (!isset($_SESSION['user'])) {
-  header('Location: login.php');
-  exit;
-}
-echo "Welcome, " . $_SESSION['user']['username'];
-?>
--- New folder/db.php
+++
@@ -1,7 +0,0 @@
-<?php
-// db.php
-$conn = new mysqli('localhost', 'root', '', 'skillhive');
-if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
-}
-?>
